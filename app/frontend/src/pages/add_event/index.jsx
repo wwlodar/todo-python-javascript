52864a8ff3a679e960f94d28d66bb02a65d4ecf3
weronika <werwlodarczyk@wp.pl>
2024-06-05 20:00:58 +0200

    Add form to event

--- app/frontend/src/pages/add_event/index.jsx
+++ app/frontend/src/pages/add_event/index.jsx
@@ -1,32 +1,111 @@
 import {useState, useEffect} from "react";
 import {Outlet} from "react-router-dom";
 import {fastapiclient} from '../../client'
+import InputForm from '../../components/InputForm'
 
 
-const GetEvent = ({id}) => {
+const AddEventForm = () => {
   const [events, setEvents] = useState([]);
-  const eventId = id
-
-  useEffect(() => {fastapiclient.getEventById(eventId)
-      .then((response) => setEvents(response));
-  }, []);
-
-  return (<a>{events}</a>
-  );
-}
+  const [error, setError] = useState({ title: '', date: ''});
+  const [backendError, setBackendError] = useState('');
+  let [isDisabled, setDisabledState] = useState(false);
+  const [eventForm, setEventForm] =  useState({ title: '', date: ''});
 
 
-const Layout = () => {
+  const onSendingEvent = (e) => {
+    e.preventDefault();
+    setLoading(true)
+    {fastapiclient.createEvent(title, date)
+    .then( (response) => {
+      setEvents(response)})
+    .catch( (err) => {
+      setLoading(false)
+      setBackendError(err);
+    });
+
+
+  const onInputChange = e => {
+      const { name, value } = e.target;
+      setEventForm(prev => ({
+        ...prev,
+        [name]: value
+      }));
+      validateInput(e);
+    }
+
+    const validateInput = e => {
+      let { name, value } = e.target;
+      setError(prev => {
+        const stateObj = { ...prev, [name]: "" };
+
+        switch (name) {
+          case "title":
+            if (!value) {
+              stateObj["title"] = "Title cannot be empty";
+              setDisabledState(true);}
+            else {
+              setDisabledState(false);
+            }
+            break;
+
+          case "date":
+            if (!value) {
+              stateObj[name] = "Date cannot be empty";
+              setDisabledState(true);
+            }
+            else {
+              setDisabledState(false);
+            }
+            break;
+
+
+          default:
+            break;
+        }
+
+        return stateObj;
+      });
+    }
+
+
+  }
+  return (
+  <form onSubmit={(e) => onSendingEvent(e)}>
+        <InputForm
+            type={"text"}
+            name={"title"}
+            label={"title"}
+            required
+            error={error.title}
+            value={eventForm.title}
+            onChange={onInputChange}
+          />
+          <InputForm
+            type={"text"}
+            name={"date"}
+            label={"date"}
+            required
+            error={error.date}
+            value={eventForm.date}
+            onChange={onInputChange}
+          />
+
+        <button title={"Add event"} error={error} loading={loading} disabled={isDisabled} className={`rounded w-full mt-4 p-1`}>Add</button>
+        <div>
+        {(backendError !== '') &&
+        <h1>{backendError.message}</h1>}
+        </div>
+
+  </form>)}}
+
+
+const GetAllEvents = () => {
   return (
     <div className="App">
-      <h2>Event</h2>
+      <h2>AddEventForm</h2>
 
-      <a>1: <GetEvent id='1' /></a>
-      <p></p>
-      <a>2: <GetEvent id='2'/></a>
-      <Outlet />
     </div>
   );
 };
 
-export default Layout;
+export default GetAllEvents;
